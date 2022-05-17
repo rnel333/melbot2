c7b593d59ccd1fac3c1cc2f4d78821b6c5a0152c
rnel333 <rnel33344@gmail.com>
2022-05-18 00:03:43 +0900

    TTS update

--- Cog/TTS.py
+++ Cog/TTS.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 
 apikey = 'b9U204z0r906S1d'
-userSpeaker = {"おてつ":13, "らいる":4, "ちょう":14, "rnel333":8}
+userSpeaker = {"412118891381391363":13, "313221694946803722":0, "502836827712126986":14, "265185184356237314":13, "502037256832286722":10}
 
 class TTS(commands.Cog):
     def __init__(self, bot):
@@ -35,35 +35,74 @@ class TTS(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.guild.voice_client:
-            if not message.author.bot:
-                if not message.content.startswith('/'):
-                    text = message.content
+        #print(message.channel.id)
+        if message.channel.id is not 781473047470669838:
+            if message.guild.voice_client:
+                if not message.author.bot:
+                    if not message.content.startswith('/'):
+                        text = message.content
 
-                    text = text.replace('\n','、')
+                        text = text.replace('\n','、')
 
-                    if text[-1:] == 'w' or text[-1:] == 'W' or text[-1:] == 'ｗ' or text[-1:] == 'W':
-                        while text[-2:-1] == 'w' or text[-2:-1] == 'W' or text[-2:-1] == 'ｗ' or text[-2:-1] == 'W':
-                            text = text[:-1]
-                        text = text[:-1] + '、ワラ'
+                        if text[-1:] == 'w' or text[-1:] == 'W' or text[-1:] == 'ｗ' or text[-1:] == 'W':
+                            while text[-2:-1] == 'w' or text[-2:-1] == 'W' or text[-2:-1] == 'ｗ' or text[-2:-1] == 'W':
+                                text = text[:-1]
+                            text = text[:-1] + '、ワラ'
 
-                    if text[-1:] == '哲':
-                        while text[-2:-1] == '哲':
-                            text = text[:-1]
-                        text = text[:-1] + 'テツ'
+                        if text[-1:] == '哲':
+                            while text[-2:-1] == '哲':
+                                text = text[:-1]
+                            text = text[:-1] + 'テツ'
+
+                        id = message.author.id
+                        if id in userSpeaker:
+                            speaker = userSpeaker[id]
+                        else:
+                            speaker = 3
                     
-                    name = message.author.name
-                    if name in userSpeaker:
-                        speaker = userSpeaker[name]
-                    else:
-                        speaker = 3
                     
+                    #print(id, speaker)
                     mp3url = f'https://api.su-shiki.com/v2/voicevox/audio/?text={text}&key={apikey}&speaker={speaker}&intonationScale=1'
                     while message.guild.voice_client.is_playing():
                         await asyncio.sleep(0.5)
                     message.guild.voice_client.play(discord.FFmpegPCMAudio(mp3url))
                 await self.bot.process_commands(message)
 
+    @commands.Cog.listener()
+    async def on_voice_state_update(self,member,before,after):
+        if before.channel is None:
+            if member.guild.voice_client is None:
+                await asyncio.sleep(0.5)
+                await after.channel.connect()
+            else:
+                if member.guild.voice_client.channel is after.channel:
+                    text = member.name + "が入室しました"
+                    mp3url = f'https://api.su-shiki.com/v2/voicevox/audio/?text={text}&key={apikey}&speaker=3&intonationScale=1'
+                    while member.guild.voice_client.is_playing():
+                        await asyncio.sleep(0.5)
+                    source = await discord.FFmpegOpusAudio.from_probe(mp3url)
+                    member.guild.voice_client.play(source)
+        elif after.channel is None:
+            if member.guild.voice_client:
+                if member.guild.voice_client.channel is before.channel:
+                    if len(member.guild.voice_client.channel.members) == 1:
+                        await asyncio.sleep(0.5)
+                        await member.guild.voice_client.disconnect()
+                    else:
+                        text = member.name + 'さんが退室しました'
+                        mp3url = f'https://api.su-shiki.com/v2/voicevox/audio/?text={text}&key={apikey}&speaker=3&intonationScale=1'
+                        while member.guild.voice_client.is_playing():
+                            await asyncio.sleep(0.5)
+                        source = await discord.FFmpegOpusAudio.from_probe(mp3url)
+                        member.guild.voice_client.play(source)
+        elif before.channel != after.channel:
+            if member.guild.voice_client:
+                if member.guild.voice_client.channel is before.channel:
+                    if len(member.guild.voice_client.channel.members) == 1 or member.voice.self_mute:
+                        await asyncio.sleep(0.5)
+                        await member.guild.voice_client.disconnect()
+                        await asyncio.sleep(0.5)
+                        await after.channel.connect()
 
 
 def setup(bot):
